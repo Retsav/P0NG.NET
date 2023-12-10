8d54aab76ce81acb0042b9437c0ef6938268e9dd
Dominik Dadański <ddadanski@edu.cdv.pl>
2023-12-10 15:26:47 +0100

    Added Gameplay State

---
+++ Assets/Scripts/GameStates/GameplayState.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayState : IGameState
+{
+    public event EventHandler OnStateChanged;
+    
+    public IGameState DoState(GameStatesSystem gss)
+    {
+        return GameStatesSystem.gameplayState;
+    }
+}
