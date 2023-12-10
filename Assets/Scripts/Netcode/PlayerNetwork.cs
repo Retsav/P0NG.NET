8290f990afaab2be0213a7564cd046ac4554fbef
Dominik Dadański <ddadanski@edu.cdv.pl>
2023-12-10 15:27:24 +0100

    Added new events to scripts for communication with game states

--- Assets/Scripts/Netcode/PlayerNetwork.cs
+++ Assets/Scripts/Netcode/PlayerNetwork.cs
@@ -7,29 +7,36 @@ using Random = UnityEngine.Random;
 
 public class PlayerNetwork : NetworkBehaviour
 {
-    private NetworkVariable<int> randomNumber = new NetworkVariable<int>(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
     [SerializeField] private LayerMask barsLayerMask;
     [SerializeField] private List<Vector3> spawnPoints;
 
     public override void OnNetworkSpawn()
     {
-        transform.position = spawnPoints[(int)OwnerClientId];
+        ScoreBar.OnAnyBarHit += ScoreBar_OnAnyBarHit;
+        ResetPlayersTransformClientRpc();
     }
 
+    private void ScoreBar_OnAnyBarHit(object sender, EventArgs e)
+    {
+        ResetPlayersTransformClientRpc();
+    }
+
+    [ClientRpc]
+    private void ResetPlayersTransformClientRpc()
+    {
+        transform.position = spawnPoints[(int)OwnerClientId];
+    }
+    
     private void Update()
     {
         if (!IsOwner) return;
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            randomNumber.Value = Random.Range(0, 100);
-        }
         Vector3 moveDir = new Vector3(0, 0, 0);
         if (Input.GetKey(KeyCode.W)) moveDir.y = +1f;
         if (Input.GetKey(KeyCode.S)) moveDir.y = -1f;
         float moveSpeed = 3f;
         float moveDistance = .51f;
-        bool canMove = !Physics2D.Raycast(transform.position, moveDir, moveDistance, barsLayerMask);
-        Debug.DrawLine(transform.position, moveDir * moveDistance, Color.red);
+        bool canMove = !Physics2D.Raycast(transform.position, moveDir, moveDistance, barsLayerMask) &&
+                       GameStatesSystem.GetCurrentState() == GameStatesSystem.gameplayState;
         if (canMove)
         {
             transform.position += moveDir * (moveSpeed * Time.deltaTime);   
