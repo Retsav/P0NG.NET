fa0aa4bdd4b97a767d944927925adb0f6aaa8b8b
Dominik Dadański <ddadanski@edu.cdv.pl>
2023-12-10 15:26:25 +0100

    Added GameplayState

---
+++ Assets/Scripts/GameStates/EntryState.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntryState : IGameState
+{
+    public event EventHandler OnStateChanged;
+
+    public IGameState DoState(GameStatesSystem gss)
+    {
+        if (!(gss.GetStateTimer() <= 0f))
+        {
+            gss.DecreaseStateTimerByDeltaTime();
+            return GameStatesSystem.entryState;
+        }
+        SendEventOnStateChangedClientRpc();
+        return GameStatesSystem.gameplayState;
+    }
+    
+    private void SendEventOnStateChangedClientRpc()
+    {
+        OnStateChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
